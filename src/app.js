01e5b647c394305c454fcd7c1c7658e863308974
Igor <sergienko339@gmail.com>
2021-04-05 20:57:45 +0300

    added my file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
-import './js/myJsCode.js';
-import './scss/base.scss';
-import './scss/typography.scss';
+import "./js/myJsCode.js";
+import "./js/testIgor.js";
+import "./scss/base.scss";
+import "./scss/typography.scss";
