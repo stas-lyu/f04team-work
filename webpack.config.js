afe0f468a3558d3dd42b063253dec78e8b9e364e
stas-lyu <staslyu1997@gmail.com>
2021-04-07 19:49:43 +0300

    handlbars add

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
@@ -41,5 +42,43 @@ module.exports = {
 
         ]
     },
-    plugins: [new MiniCssExtractPlugin()],
+        plugins: [
+            new MiniCssExtractPlugin(),
+
+            new HandlebarsPlugin({
+                // path to hbs entry file(s). Also supports nested directories if write path.join(process.cwd(), "app", "src", "**", "*.hbs"),
+                entry: path.join(process.cwd(), "app", "src", "*.hbs"),
+                // output path and filename(s). This should lie within the webpacks output-folder
+                // if ommited, the input filepath stripped of its extension will be used
+                output: path.join(process.cwd(), "build", "[name].html"),
+                // you can also add a [path] variable, which will emit the files with their relative path, like
+                // output: path.join(process.cwd(), "build", [path], "[name].html"),
+
+                // data passed to main hbs template: `main-template(data)`
+                // data: require("./app/data/project.json"),
+                // or add it as filepath to rebuild data on change using webpack-dev-server
+                data: path.join(__dirname, "app/data/project.json"),
+
+                // globbed path to partials, where folder/filename is unique
+                partials: [
+                    path.join(process.cwd(), "app", "src", "components", "*", "*.hbs")
+                ],
+
+                // register custom helpers. May be either a function or a glob-pattern
+                helpers: {
+                    nameOfHbsHelper: Function.prototype,
+                    projectHelpers: path.join(process.cwd(), "app", "helpers", "*.helper.js")
+                },
+
+                // hooks
+                // getTargetFilepath: function (filepath, outputTemplate) {},
+                // getPartialId: function (filePath) {}
+                onBeforeSetup: function (Handlebars) {},
+                onBeforeAddPartials: function (Handlebars, partialsMap) {},
+                onBeforeCompile: function (Handlebars, templateContent) {},
+                onBeforeRender: function (Handlebars, data, filename) {},
+                onBeforeSave: function (Handlebars, resultHtml, filename) {},
+                onDone: function (Handlebars, filename) {}
+            })
+        ]
 };
